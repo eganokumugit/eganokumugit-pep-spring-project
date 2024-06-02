f2bcd6c0589b08b8029a0535b3e70864b36d0537
Egan Okumu <eganokumu@gmail.com>
2024-06-02 05:15:16 +0000

    MessageRepo v1

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,8 +1,18 @@
 package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
+import java.util.List;
+
+@Repository
 public interface MessageRepository extends JpaRepository<Message,Long>
 {
+    @Query("FROM message m, account a WHERE m.postedBy = a.accountId AND m.postedBy = :postedBy")
+    List<Message> getAllMessagesFromUser(@Param("postedBy") Long postedBy);
+
+    public Message findMessageByMessageTextAndPostedByAndTimePostedEpoch(String messageText, Integer postedBy, Long timePostedEpoch);
 }
