5406037bd41e53b0e4e96424585f354f03bc19dc
Egan Okumu <eganokumu@gmail.com>
2024-06-02 04:03:48 +0000

    Account Service v1

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,43 @@
 package com.example.service;
+import java.util.HashMap;
 
-public class AccountService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+@Service
+public class AccountService 
+{
+    AccountRepository accRepo;
+    @Autowired
+    public AccountService(AccountRepository accRepo)
+    {
+        this.accRepo = accRepo;
+    }
+
+    public HashMap<Account,Integer> registerAccount(Account acc)
+    {
+        HashMap<Account,Integer> resultMap = new HashMap<>();
+        if(accRepo.findAccountByUsername(acc) == null && acc.getPassword().length() > 4 && acc.getUsername().length() > 0)
+        {
+            accRepo.save(acc);
+            Account registeredAcc = accRepo.findAccountByUsername(acc);
+            resultMap.put(registeredAcc, 200);
+            return resultMap;
+        }
+        else if(accRepo.findAccountByUsername(acc) != null)
+        {
+            resultMap.put(null, 409);
+            return resultMap;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public Account loginUser(Account acc)
+    {
+        return accRepo.findAccountByUsernameAndPassword(acc);   
+    }
 }
