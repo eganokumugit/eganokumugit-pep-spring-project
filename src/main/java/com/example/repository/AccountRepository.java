4c924f54ec65744f6648f2d0c0c63d6811737b2d
Egan Okumu <eganokumu@gmail.com>
2024-06-02 04:03:35 +0000

    AccountRepository v2

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -5,4 +5,7 @@ import com.example.entity.Account;
 
 public interface AccountRepository extends JpaRepository<Account,Long>
 {
+    public Account findAccountByUsername(Account acc);
+
+    public Account findAccountByUsernameAndPassword(Account acc);
 }
