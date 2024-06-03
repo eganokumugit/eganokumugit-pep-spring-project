c675834a07e3e71fec08536d0d845b77defd90d5
Egan Okumu <eganokumu@gmail.com>
2024-06-03 04:09:02 +0000

    SMC v2

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -5,10 +5,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,13 +52,77 @@ public class SocialMediaController
         }
     }
 
-/*
-    app.post("/register", this:: postRegistrationHandler);
-    app.post("/login", this:: postLoginRegistrationHandler);
-    app.post("/messages", this:: postNewMessageHandler);
-    app.get("/messages", this:: getMessageHandler);
-    app.get("/messages/{message_id}", this:: getMessageWithIdHandler);
-    app.delete("/messages/{message_id}", this:: deleteMessageHandler);
-    app.get("/accounts/{account_id}/messages", this:: getMessagesFromAccountHandler);
-    app.patch("/messages/{message_id}", this :: updateMessageHandler);*/
+    @PostMapping("/login")
+    public ResponseEntity<String> postLoginRegistrationHandler(@RequestBody Account acc) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Account loggedIn = accService.loginUser(acc);
+        if(loggedIn == null)
+        {
+            return ResponseEntity.status(400).body("");
+        }
+        else
+        {
+            String jsonResponse = mapper.writeValueAsString(loggedIn);
+            return ResponseEntity.status(200).body(jsonResponse);
+        }
+    }
+    @PostMapping("/messages")
+    public ResponseEntity<String> postNewMessageHandler(@RequestBody Message msg) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Message newMessage = msgService.addMessage(msg);
+        if(newMessage == null)
+        {
+            return ResponseEntity.status(400).body("");
+        }
+        else
+        {
+            String jsonResponse = mapper.writeValueAsString(newMessage);
+            return ResponseEntity.status(200).body(jsonResponse);
+        }
+    }
+
+
+    @GetMapping("/messages")
+    public ResponseEntity<String> getMessageHandler() throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Message> msgList = msgService.getAllMessages();
+        String jsonResponse = mapper.writeValueAsString(msgList);
+        return ResponseEntity.status(200).body(jsonResponse); 
+    }
+    @GetMapping("/messages/{message_id}")
+    public ResponseEntity<String> getMessageWithIdHandler(@RequestBody Long msgId) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Message msg = msgService.getMessageById(msgId);
+        String jsonResponse = mapper.writeValueAsString(msg);
+        return ResponseEntity.status(200).body(jsonResponse); 
+    }
+    
+    @DeleteMapping("/messages/{message_id}")
+    public ResponseEntity<String> deleteMessageHandler(@RequestBody Long msgId) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Integer delete = msgService.deleteMessage(msgId);
+        String jsonResponse = mapper.writeValueAsString(delete);
+        return ResponseEntity.status(200).body(jsonResponse); 
+    }
+
+    @PatchMapping("/messages/{message_id}")
+    public ResponseEntity<String> updateMessageHandler(@RequestBody Long msgId, String newMsg) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Integer update = msgService.updateMessage(msgId, newMsg);
+        if(update == null)
+        {
+            return ResponseEntity.status(400).body("");
+        }
+        else
+        {
+            String jsonResponse = mapper.writeValueAsString(update);
+            return ResponseEntity.status(200).body(jsonResponse);
+        }
+    }
 }
